15a6cb37df6c417f82e2935ac47a6c10cb212529
Saleh DASSOUKHI <57576759+dassoukhi@users.noreply.github.com>
2020-12-04 14:55:34 +0100

    Test ci-cd

---
+++ target/generated-sources/annotations/com/ynov/jh/demo/domain/Emplacement_.java
@@ -0,0 +1,18 @@
+package com.ynov.jh.demo.domain;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value = "org.hibernate.jpamodelgen.JPAMetaModelEntityProcessor")
+@StaticMetamodel(Emplacement.class)
+public abstract class Emplacement_ {
+
+	public static volatile SingularAttribute<Emplacement, String> nomEplacement;
+	public static volatile SingularAttribute<Emplacement, Long> id;
+
+	public static final String NOM_EPLACEMENT = "nomEplacement";
+	public static final String ID = "id";
+
+}
+
